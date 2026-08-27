42bf50dde3d4f80e830cfa74a5e17a3a164e99cd
fikrianamm <fikritaufiqulanam1@gmail.com>
2026-08-27 09:10:59 +0700

    func, input, if else

---
+++ bin/cli.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+const version = "0.0.1";
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == "help") {
+    printUsage();
+  } else if (arguments.first == "version") {
+    print("Dart version $version");
+  } else if (arguments.first == "search") {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print(' Please provide an article title');
+
+    articleTitle = stdin.readLineSync() ?? "";
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
