d46c2a8e92be486088e2b607c3b5ae284895ed74
fikrianamm <fikritaufiqulanam1@gmail.com>
2026-08-30 16:10:16 +0700

    oop dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,60 +1,8 @@
-import 'dart:io';
+import 'package:command_runner/command_runner.dart';
 
-import 'package:http/http.dart' as http;
-
-const version = "0.0.1";
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == "help") {
-    printUsage();
-  } else if (arguments.first == "version") {
-    print("Dart version $version");
-  } else if (arguments.first == "wikipedia") {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
-}
-
-void printUsage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
-  );
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  if (arguments == null || arguments.isEmpty) {
-    print(' Please provide an article title');
-
-    final inputFormStdin = stdin.readLineSync();
-    if (inputFormStdin == null || inputFormStdin.isEmpty) {
-      print('No article title provided. Exiting...');
-      return;
-    }
-    articleTitle = inputFormStdin;
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org', // Wikipedia API domain
-    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
-  );
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
